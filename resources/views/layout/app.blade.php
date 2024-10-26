c1973570aa7d0925a384882e77dce75c2c84f333
ardianrachman9 <ardianrachman9@gmail.com>
2024-10-26 13:56:24 +0700

    modified:   resources/views/layout/app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @include('layout.style')
-    <title>CRUD KTP-KK</title>
+    <title>TEST | CRUD KTP-KK</title>
 </head>
 <body>
     @include('layout.partials.navbar')
